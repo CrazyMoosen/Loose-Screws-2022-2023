eb6425ae036b784165c617be287c3badc9b0ffb3
Teja D <62719821+tejaDhulipala@users.noreply.github.com>
2022-10-18 19:44:44 -0400

    Add files via upload

---
+++ Team22012/src/main/java/org/firstinspires/ftc/team22012/subsystems/ClawSubsystem.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.team22012.universal.subsystems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class ClawSubsystem extends SubsystemBase {
+
+    HardwareMap hardwareMap;
+
+    //initialize claw
+    public ClawSubsystem(final HardwareMap hardwareMap) {
+
+    }
+
+    //function that closes the gripper, parameter is the value taken to grab the cone
+    public void close(double value) {
+
+    }
+
+    //function that releases the cone from the grip of the gripper
+    public void release() {
+
+    }
+
+}
