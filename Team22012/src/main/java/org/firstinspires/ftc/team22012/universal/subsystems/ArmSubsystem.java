b330da01150c6389fe2af1bf859f945c15ad1e4d
Ismailnotsus <91224837+Ismailnotsus@users.noreply.github.com>
2022-10-22 13:55:28 -0400

    Added Arm methods

--- Team22012/src/main/java/org/firstinspires/ftc/team22012/universal/subsystems/ArmSubsystem.java
+++ Team22012/src/main/java/org/firstinspires/ftc/team22012/universal/subsystems/ArmSubsystem.java
@@ -3,9 +3,28 @@ package org.firstinspires.ftc.team22012.universal.subsystems;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class ArmSubsystem {
+    private Motor S1, S2;
 
     public ArmSubsystem(HardwareMap hardwareMap) {
 
+    }
+    public void completeExtend(){
+        S1.setRunMode(Motor.RunMode.RawPower);
+        S1.set(0.75);
+        S2.setRunMode(Motor.RunMode.RawPower);
+        S2.set(0.75);
+    }
+    public void ArmMove(double height){
+        S1.setRunMode(Motor.RunMode.RawPower);
+        S1.set(height);
+        S2.setRunMode(Motor.RunMode.RawPower);
+        S2.set(height);
+    }
+    public void completeRetract(){
+        S1.setRunMode(Motor.RunMode.RawPower);
+        S1.set(-0.75);
+        S2.setRunMode(Motor.RunMode.RawPower);
+        S2.set(-0.75);
     }
 
 }
