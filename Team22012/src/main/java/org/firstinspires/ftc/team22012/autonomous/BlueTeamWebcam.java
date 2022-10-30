105b998011ea342a73c8201a03024a8944c81bda
ismailnotsus <ismailakdenizorlando@gmail.com>
2022-10-29 21:25:05 -0400

    Autonomous now can turn and move properly(no it cant), while the BlueTeamWebcam.java also works now for signal sleeve colors(no it does not)

--- Team22012/src/main/java/org/firstinspires/ftc/team22012/autonomous/BlueTeamWebcam.java
+++ Team22012/src/main/java/org/firstinspires/ftc/team22012/autonomous/BlueTeamWebcam.java
@@ -1,30 +1,31 @@
 package org.firstinspires.ftc.team22012.autonomous;
 
+import static java.lang.Math.abs;
+
 import android.graphics.Bitmap;
 
+import com.arcrobotics.ftclib.drivebase.MecanumDrive;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
-import org.firstinspires.ftc.team22012.vision.MonishPython;
 import org.firstinspires.ftc.team22012.vision.VuforiaLocalizerImplSubclass;
 import org.opencv.android.Utils;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfPoint;
-import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
-import java.sql.Array;
-import java.util.ArrayList;
 import java.util.List;
 
 @Autonomous(name = "Vuforia Webcam Detection Blue Team")
@@ -33,9 +34,18 @@ public class BlueTeamWebcam extends LinearOpMode{
         GREEN,
         YELLOW,
         PURPLE,
-        NONE
+        NONE;
     }
     private SignalSleeveLocation location;
+    double percent = 0;
+    Motor fL, fR, bL, bR;
+    ElapsedTime elapsedTime = new ElapsedTime();
+
+    // Experimentally determined variables
+    double speed = 50.5; // In inches/sec
+    double stoppingDistance = 1; // In inches, the distance it takes to stop the robot travelling
+    // at the power of 0.8
+    double degPerSec = 160;
     /**
      * Specify the source for the Tensor Flow Model.
      * If the TensorFlowLite object model is included in the Robot Controller App as an "asset",
@@ -51,6 +61,9 @@ public class BlueTeamWebcam extends LinearOpMode{
             "2 Bulb",
             "3 Panel"
     };
+
+    int run = 0;
+    boolean moved = false;
     /**
      * IMPORTANT: You need to obtain your own license key to use Vuforia. The string below with which
      * 'parameters.vuforiaLicenseKey' is initialized is for illustration only, and will not function.
@@ -80,6 +93,17 @@ public class BlueTeamWebcam extends LinearOpMode{
 
     @Override
     public void runOpMode() {
+        fL = new Motor(hardwareMap, "fL", Motor.GoBILDA.RPM_312);
+        fR = new Motor(hardwareMap, "fR", Motor.GoBILDA.RPM_312);
+        bL = new Motor(hardwareMap, "bL", Motor.GoBILDA.RPM_312);
+        bR = new Motor(hardwareMap, "bR", Motor.GoBILDA.RPM_312);
+
+        MecanumDrive mecanumDrive = new MecanumDrive(fL, fR, bL, bR);
+
+        fL.setDistancePerPulse(18);
+        fR.setDistancePerPulse(18);
+        bR.setDistancePerPulse(18);
+        bL.setDistancePerPulse(18);
         // The TFObjectDetector uses the camera frames from the VuforiaLocalizer, so we create that
         // first.
         initVuforia();
@@ -133,61 +157,75 @@ public class BlueTeamWebcam extends LinearOpMode{
                     //Scalar blue_upper = new Scalar(128.0833D, 255, 255);
 
                     //trying to detect purple:
-                    Scalar purple_lower = new Scalar(100, 62, 128);
+                    Mat ROI = img.submat(new Rect(new Point(225, 300), new Point(400, 480)));
+                    String roiPath = "sdcard/FIRST/roiPath.png";
+                    Imgcodecs.imwrite(roiPath, ROI);
+
+                    Scalar purple_lower = new Scalar(100, 50, 75);
                     Scalar purple_upper = new Scalar(255, 255, 255);
 
                     //trying to get yellow balls
-                    Scalar yellow_lower = new Scalar(0,62, 128);
-                    Scalar yellow_upper = new Scalar(45, 255, 255);
+                    Scalar yellow_lower = new Scalar(0, 62, 128);
+                    Scalar yellow_upper = new Scalar(45, 255, 175);
                     //trying to get green
-                    Scalar green_lower = new Scalar(45,62, 0);
-                    Scalar green_upper = new Scalar(90, 255, 128);
+                    Scalar green_lower = new Scalar(45, 0, 0);
+                    Scalar green_upper = new Scalar(90, 255, 100);
                     //input image converts to HSV
-                    Mat hsvPic = new Mat();
-                    Imgproc.cvtColor(img, hsvPic, Imgproc.COLOR_RGB2HSV);
+                    //Mat hsvPic = new Mat();
+                    //Imgproc.cvtColor(img, hsvPic, Imgproc.COLOR_RGB2HSV);
+
+                    Mat ROIHsv = new Mat();
+                    Imgproc.cvtColor(ROI, ROIHsv, Imgproc.COLOR_RGB2HSV);
 
                     //the thresholded frame that is black and white and shows the blue in the picture.
+                    //Mat threshold = new Mat();
+                    //Core.inRange(hsvPic, purple_lower, purple_upper, threshold);
+
                     Mat threshold = new Mat();
-                    Core.inRange(hsvPic, purple_lower, purple_upper, threshold);
+                    Core.inRange(ROIHsv, purple_lower, purple_upper, threshold);
+                    //Core.inRange(ROIHsv, purple_lower, purple_upper, ROIThresh);
+                    //Core.inRange(ROIHsv, purple_lower, purple_upper, ROIThresh);
+
+                    String roiThreshPath = "sdcard/FIRST/roiThreshPath.png";
+                    Imgcodecs.imwrite(roiThreshPath, threshold);
 
                     Mat threshold2 = new Mat();
-                    Core.inRange(hsvPic, yellow_lower, yellow_upper, threshold2);
+                    //Core.inRange(hsvPic, yellow_lower, yellow_upper, threshold2);
+                    Core.inRange(ROIHsv, yellow_lower, yellow_upper, threshold2);
 
                     Mat threshold3 = new Mat();
-                    Core.inRange(hsvPic, green_lower, green_upper, threshold3);
+                    //Core.inRange(hsvPic, green_lower, green_upper, threshold3);
+                    Core.inRange(ROIHsv, green_lower, green_upper, threshold3);
 
                     long whitepixels3 =Core.countNonZero(threshold3);
-                    long totalpixels3 = threshold3.total();
+                    long total = ROIHsv.total();
                     long whitepixels2 =Core.countNonZero(threshold2);
-                    long totalpixels2 = threshold2.total();
                     long whitepixels1 =Core.countNonZero(threshold);
-                    long totalpixels1 = threshold.total();
-                    if (whitepixels1>whitepixels2 && whitepixels1>whitepixels3){
-                        location=SignalSleeveLocation.PURPLE;
-                    } else if (whitepixels2>whitepixels1 && whitepixels2>whitepixels3){
+                    if (whitepixels1>whitepixels2 && whitepixels1>whitepixels3 && whitepixels1*100/total >= 20) {
+                        location = SignalSleeveLocation.PURPLE;
+                        percent = whitepixels1*100/total;
+                    } else if (whitepixels2>whitepixels1 && whitepixels2>whitepixels3 && whitepixels2*100/total >= 20) {
                         location=SignalSleeveLocation.YELLOW;
-                    } else if(whitepixels3>whitepixels1 && whitepixels3>whitepixels2){
+                        percent = whitepixels2*100/total;
+                    } else if (whitepixels3>whitepixels1 && whitepixels3>whitepixels2 && whitepixels3*100/total >= 20) {
                         location=SignalSleeveLocation.GREEN;
-                    } else{
-                        location=SignalSleeveLocation.NONE;
+                        percent = whitepixels3*100/total;
+                    } else {
+                        location = SignalSleeveLocation.NONE;
+                        percent = 0;
                     }
 
-
-
-                    String countourpath = "sdcard/FIRST/counterpath.png";
-                    Imgcodecs.imwrite(countourpath, img);
-
-                    String rgbPath = "sdcard/FIRST/rgbFile.png";
-                    Imgcodecs.imwrite(rgbPath, hsvPic);
+                    //String rgbPath = "sdcard/FIRST/rgbFile.png";
+                    //Imgcodecs.imwrite(rgbPath, hsvPic);
 
                     //thresholded images should be saved here
-                    String filePath = "sdcard/FIRST/thresholdFile.png";
-                    Imgcodecs.imwrite(filePath, threshold);
+                    //String filePath = "sdcard/FIRST/thresholdFile.png";
+                    //Imgcodecs.imwrite(filePath, threshold);
 
-                    String filePath2 = "sdcard/FIRST/threshold2File.png";
-                    Imgcodecs.imwrite(filePath2, threshold2);
-                    String filePath3 = "sdcard/FIRST/threshold3File.png";
-                    Imgcodecs.imwrite(filePath3, threshold3);
+                    //String filePath2 = "sdcard/FIRST/threshold2File.png";
+                    //Imgcodecs.imwrite(filePath2, threshold2);
+                    //String filePath3 = "sdcard/FIRST/threshold3File.png";
+                    //Imgcodecs.imwrite(filePath3, threshold3);
                     //use this for extra help: http://overchargedrobotics.org/wp-content/uploads/2018/08/Advanced-Programming-Vision.pdf
 
                 }
@@ -223,11 +261,152 @@ public class BlueTeamWebcam extends LinearOpMode{
                         if (location==SignalSleeveLocation.NONE){
                             telemetry.addData("location is", "none");
                         }
+                        telemetry.addData("Percentage of color", percent);
                         telemetry.update();
                     }
                 }
+
+                //move bobot
+                if(!moved) {
+                    if (run < 10 || location != SignalSleeveLocation.NONE) {
+                        if (location == SignalSleeveLocation.GREEN) {
+                            moveLinear(0.8, 19);
+                            moveLinear(-0.8, stoppingDistance);
+                            moved = true;
+                        }
+                        if (location == SignalSleeveLocation.PURPLE) {
+                            moveLinear(0.8, 19);
+                            moveLinear(-0.8, stoppingDistance);
+                            turn(-0.8, 60);
+                            moveLinear(0.8, 16);
+                            moveLinear(-0.8, stoppingDistance);
+                            moved = true;
+                        }
+                        if (location == SignalSleeveLocation.YELLOW) {
+                            moveLinear(0.8, 19);
+                            moveLinear(-0.8, stoppingDistance);
+                            turn(0.8, 60);
+                            moveLinear(0.8, 16);
+                            moveLinear(-0.8, stoppingDistance);
+                            moved = true;
+                        }
+                        if (location == SignalSleeveLocation.NONE) {
+                        }
+                    }
+                }
+                run++;
+            }
+
+//if (location==SignalSleeveLocation.GREEN){
+            //    moveLinear(-0.8, 19);
+            //    moveLinear(0.8, stoppingDistance);
+            //}
+            //if (location==SignalSleeveLocation.PURPLE){
+            //    moveLinear(0.8, 19);
+            //    moveLinear(-0.8, stoppingDistance);
+            //turn(-0.8, 80);
+            //moveLinear(0.8, 16);
+            //moveLinear(-0.8, stoppingDistance);
+            //}
+//                if (location==SignalSleeveLocation.YELLOW){
+//                    moveLinear(0.8, 19);
+//                    moveLinear(0.8, stoppingDistance);
+//                    turn(-0.8, 80);
+//                    moveLinear(-0.8, 16);
+//                    moveLinear(0.8, stoppingDistance);
+//                }
+            if (location==SignalSleeveLocation.NONE){
+
             }
         }
+    }
+
+    public void moveLinear(double power, double distance) {
+        // This code will move backward if the power is negative
+        // Whenever you call this code add another moveLinear thing for the opposite power and
+        // stopping distance
+        double time = distance / (speed * abs(power));
+        elapsedTime.reset();
+        while (elapsedTime.milliseconds() < time * 1000) {
+            fL.set(-power);
+            fR.set(power);
+            bL.set(-power);
+            bR.set(power);
+        }
+        fL.set(0);
+        fR.set(0);
+        bR.set(0);
+        bL.set(0);
+    }
+
+    public void strafeLinear(double power, double distance) {
+        double time = distance / (speed * abs(power));
+        elapsedTime.reset();
+        MecanumDrive mecanumDrive = new MecanumDrive(fL, fR, bL, bR);
+        while (elapsedTime.milliseconds() < time * 1000) {
+            mecanumDrive.driveRobotCentric(-power, 0, 0);
+        }
+        mecanumDrive.driveRobotCentric(0, 0, 0);
+    }
+
+
+    public void moveLinearTime(double power, double time) {
+        // This code will move backward if the power is negative
+        elapsedTime.reset();
+        while (elapsedTime.milliseconds() < time * 1000) {
+            fL.set(-power);
+            fR.set(power);
+            bL.set(-power);
+            bR.set(power);
+        }
+        fL.set(0);
+        fR.set(0);
+        bR.set(0);
+        bL.set(0);
+    }
+
+    public void moveRight(double power, double distance){
+        double time = distance / (speed * abs(power));
+        elapsedTime.reset();
+        while (elapsedTime.milliseconds() < time * 1000) {
+            fL.set(-power);
+            fR.set(power);
+            bL.set(power);
+            bR.set(-power);
+        }
+        fL.set(0);
+        fR.set(0);
+        bR.set(0);
+        bL.set(0);
+    }
+
+    public void turnForSec(double power, double time){
+        elapsedTime.reset();
+        while (elapsedTime.milliseconds() < time * 1000) {
+            fL.set(power);
+            fR.set(power);
+            bL.set(power);
+            bR.set(power);
+        }
+        fL.set(0);
+        fR.set(0);
+        bR.set(0);
+        bL.set(0);
+    }
+
+    public void turn(double power, double angle){
+        elapsedTime.reset();
+        double time  = angle / degPerSec;
+        while (elapsedTime.milliseconds() < time * 1000) {
+            fL.set(power);
+            fR.set(power);
+            bL.set(power);
+            bR.set(power);
+        }
+        fL.set(0);
+        fR.set(0);
+        bR.set(0);
+        bL.set(0);
     }
 
     /**
--- Team22012/src/main/java/org/firstinspires/ftc/team22012/teleoperated/RobotCentricTeleOp.java
+++ Team22012/src/main/java/org/firstinspires/ftc/team22012/teleoperated/RobotCentricTeleOp.java
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.team22012.teleoperated;
 
 import com.arcrobotics.ftclib.drivebase.MecanumDrive;
+import com.arcrobotics.ftclib.drivebase.RobotDrive;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.geometry.Vector2d;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -11,6 +13,14 @@ import org.firstinspires.ftc.team22012.universal.subsystems.ClawSubsystem;
 
 @TeleOp(name="RobotCentric", group = "Drive Modes")
 public class RobotCentricTeleOp extends OpMode {
+
+    public static final double kDefaultRangeMin = -1.0;
+    public static final double kDefaultRangeMax = 1.0;
+    public static final double kDefaultMaxSpeed = 1.0;
+
+    protected double rangeMin = kDefaultRangeMin;
+    protected double rangeMax = kDefaultRangeMax;
+    protected double maxOutput = kDefaultMaxSpeed;
 
     private Motor fL, fR, bL, bR;
     private MecanumDrive mecanumDrive;
@@ -36,17 +46,84 @@ public class RobotCentricTeleOp extends OpMode {
 
         shreyController = new GamepadEx(gamepad1);
         //monishController = new GamepadEx(gamepad2);
-
-        //claw = new ClawSubsystem(hardwareMap);
-        //arm = new ArmSubsystem(hardwareMap);
+        //claw = new ClawSubArmSubsystemsystem(hardwareMap);
+        //arm = new (hardwareMap);
     }
 
     @Override
     public void loop() {
         mecanumDrive.driveRobotCentric(
-                shreyController.getLeftX()*0.65,
+                -shreyController.getLeftX()*0.65,
                 -shreyController.getLeftY()*0.65,
                 -shreyController.getRightX()*0.65
         );
     }
+
+//    @Override
+//    public void loop() {
+//        double strafeSpeed = clipRange(shreyController.getLeftX() * 0.65);
+//        double forwardSpeed = clipRange(-shreyController.getLeftY() * 0.65);
+//        double turnSpeed = clipRange(-shreyController.getRightX() * 0.65);
+//
+//        Vector2d input = new Vector2d(strafeSpeed, forwardSpeed);
+//        input = input.rotateBy(0.0);
+//
+//        double theta = input.angle();
+//
+//        double[] wheelSpeeds = new double[4];
+//        wheelSpeeds[RobotDrive.MotorType.kFrontLeft.value] = Math.sin(theta + Math.PI / 4);
+//        wheelSpeeds[RobotDrive.MotorType.kFrontRight.value] = Math.sin(theta - Math.PI / 4);
+//        wheelSpeeds[RobotDrive.MotorType.kBackLeft.value] = Math.sin(theta - Math.PI / 4);
+//        wheelSpeeds[RobotDrive.MotorType.kBackRight.value] = Math.sin(theta + Math.PI / 4);
+//
+//        normalize(wheelSpeeds, input.magnitude());
+//
+//        wheelSpeeds[RobotDrive.MotorType.kFrontLeft.value] += turnSpeed;
+//        wheelSpeeds[RobotDrive.MotorType.kFrontRight.value] -= turnSpeed;
+//        wheelSpeeds[RobotDrive.MotorType.kBackLeft.value] += turnSpeed;
+//        wheelSpeeds[RobotDrive.MotorType.kBackRight.value] -= turnSpeed;
+//
+//        normalize(wheelSpeeds);
+//
+//        mecanumDrive.driveWithMotorPowers(
+//                wheelSpeeds[RobotDrive.MotorType.kFrontLeft.value * 3 / 4],
+//                wheelSpeeds[RobotDrive.MotorType.kFrontRight.value * 3 / 4],
+//                wheelSpeeds[RobotDrive.MotorType.kBackLeft.value],
+//                wheelSpeeds[RobotDrive.MotorType.kBackRight.value]
+//        );
+//    }
+//
+//    public double clipRange(double value) {
+//        return value <= rangeMin ? rangeMin
+//                : Math.min(value, rangeMax);
+//    }
+//
+//    void normalize(double[] wheelSpeeds, double magnitude) {
+//        double maxMagnitude = Math.abs(wheelSpeeds[0]);
+//        for (int i = 1; i < wheelSpeeds.length; i++) {
+//            double temp = Math.abs(wheelSpeeds[i]);
+//            if (maxMagnitude < temp) {
+//                maxMagnitude = temp;
+//            }
+//        }
+//        for (int i = 0; i < wheelSpeeds.length; i++) {
+//            wheelSpeeds[i] = (wheelSpeeds[i] / maxMagnitude) * magnitude;
+//        }
+//
+//    }
+//
+//    void normalize(double[] wheelSpeeds) {
+//        double maxMagnitude = Math.abs(wheelSpeeds[0]);
+//        for (int i = 1; i < wheelSpeeds.length; i++) {
+//            double temp = Math.abs(wheelSpeeds[i]);
+//            if (maxMagnitude < temp) {
+//                maxMagnitude = temp;
+//            }
+//        }
+//        if (maxMagnitude > 1) {
+//            for (int i = 0; i < wheelSpeeds.length; i++) {
+//                wheelSpeeds[i] = (wheelSpeeds[i] / maxMagnitude);
+//            }
+//        }
+
 }
